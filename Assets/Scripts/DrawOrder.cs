71fc88ac310b634dc32c9b91a26146bcfca371ba
unknown <t3d.y34t3s@gmail.com>
2018-03-20 14:11:22 +0000

    Add script to set sortingorder so you can go behind buildings

---
+++ Assets/Scripts/DrawOrder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawOrder : MonoBehaviour {
+
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update () {
+        sr.sortingOrder = Mathf.RoundToInt(transform.position.y / 3f) * -1;
+    }
+}
