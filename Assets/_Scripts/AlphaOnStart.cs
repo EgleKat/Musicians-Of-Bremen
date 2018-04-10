0cbe12856f4219c82f9fe8b40e98bb232c7537b3
rookwood101 <alexandersadler1@gmail.com>
2018-04-10 17:42:44 +0100

    Add alphaonstart script to make invisible things visible

---
+++ Assets/_Scripts/AlphaOnStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlphaOnStart : MonoBehaviour
+{
+    void Awake()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Image image = GetComponent<Image>();
+
+        if (sr)
+        {
+            Color c = sr.color;
+            c.a = 1;
+            sr.color = c;
+        }
+        if (image)
+        {
+            Color c = image.color;
+            c.a = 1;
+            image.color = c;
+        }
+    }
+}
